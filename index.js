e78c34a9f7143ba8d770d38eee7486b760864a8c
Juan Arias <jcamiloat@gmail.com>
2022-09-20 14:23:48 -0500

    Ex 3.4 Phonebook Backend Step04

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ app.get('/api/contacts/:id', (req, res) => {
   }
 });
 
+app.delete('/api/contacts/:id', (req, res) => {
+  console.log('Deleting single contact');
+  const id = Number(req.params.id);
+  contacts = contacts.filter((contact) => contact.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
